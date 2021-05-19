ecc568d4838ea6860e62e66da06afbd87433297d
Raphael Cerqueira <raphaelfc@gmail.com>
2021-05-19 20:15:46 -0300

    Initial Commit

---
+++ RedditAppRedesign/RedditAppRedesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedditAppRedesignApp.swift
+//  RedditAppRedesign
+//
+//  Created by Raphael Cerqueira on 19/05/21.
+//
+
+import SwiftUI
+
+@main
+struct RedditAppRedesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
